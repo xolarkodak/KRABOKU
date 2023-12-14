759dc0fcf206818bc8f235d6f2985a6585171e80
Bond Vlad <wlad23072003@gmail.com>
2023-12-14 15:56:16 +0200

    Film_page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import HomeScreen from "./Screens/HomeScreen";
-import AboutUs from "./Screens/AboutUs";
-import ContactUs from "./Screens/ContactUs";
-import MoviesPage from "./Screens/Movies";
-import NotFound from "./Screens/NotFound";
+import HomeScreen from './Screens/HomeScreen';
+import AboutUs from './Screens/AboutUs';
+import ContactUs from './Screens/ContactUs';
+import MoviesPage from './Screens/Movies';
+import NotFound from './Screens/NotFound';
+import SingleMovie from './Screens/SingleMovie';
 
 function App() {
-  return(<>
-    <Routes>
-    <Route path="/" element={<HomeScreen />} />
-    <Route path="/about-us" element={<AboutUs />} />
-    <Route path="/contact-us" element={<ContactUs />} />
-    <Route path="/movies" element={<MoviesPage />} />
-    {/* <Route path="/movies/:search" element={<MoviesPage />} />
-    <Route path="/movie/:id" element={<SingleMovie />} />
-    <Route path="/watch/:id" element={<WatchPage />} />
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movie/:id" element={<SingleMovie />} />
+        {/* <Route path="/watch/:id" element={<WatchPage />} />
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} /> */}
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-  </>
-    );
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
