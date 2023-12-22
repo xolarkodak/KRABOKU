b4a3f07342e0d405160b49a002ed7b8f2cef92cd
MarioKarv <mariook0204@gmail.com>
2023-12-22 13:11:05 +0200

    share modal + movie cast + movie valid

--- client/src/Components/Modals/ShareModal.js
+++ client/src/Components/Modals/ShareModal.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import MainModal from './MainModal';
-import { FaFacebook, FaPinterest, FaTelegram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import React from "react";
+import MainModal from "./MainModal";
+import {
+  FaFacebook,
+  FaPinterest,
+  FaTelegram,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -8,8 +14,8 @@ import {
   TelegramShareButton,
   TwitterShareButton,
   WhatsappShareButton,
-} from 'react-share';
-import { MdEmail } from 'react-icons/md';
+} from "react-share";
+import { MdEmail } from "react-icons/md";
 
 function ShareMovieModal({ modalOpen, setModalOpen, movie }) {
   const shareData = [
@@ -39,16 +45,20 @@ function ShareMovieModal({ modalOpen, setModalOpen, movie }) {
     },
   ];
 
-  const url = `${window.location.protocol}//${window.location.host}/movie/${movie.name}`;
+  const url = `${window.location.protocol}//${window.location.host}/movie/${movie?._id}`;
   return (
     <MainModal modalOpen={modalOpen} setModalOpen={setModalOpen}>
       <div className="inline-block sm:w-4/5 border border-border md:w-3/5 lg:w-2/5 w-full align-middle p-10 overflow-y-auto h-full bg-main text-white rounded-2xl">
         <h2 className="text-2xl">
-          Поділитись <span className="text-xl font-bold">"{movie?.name}"</span>
+          Поділитися <span className="text-xl font-bold">"{movie?.name}"</span>
         </h2>
         <form className="flex-rows flex-wrap gap-6 mt-6">
           {shareData.map((data, index) => (
-            <data.shareButton key={index} url={url} quote="Kraboku | Безкоштовно для всіх">
+            <data.shareButton
+              key={index}
+              url={url}
+              quote="KRABOKU"
+            >
               <div className="w-12 transitions hover:bg-subMain flex-colo text-lg h-12 bg-white rounded bg-opacity-30">
                 <data.icon />
               </div>
--- client/src/Components/Single/MovieRates.js
+++ client/src/Components/Single/MovieRates.js
@@ -1,86 +1,161 @@
-import React, { useState } from 'react';
-import Titles from '../Titles';
-import { BsBookmarkStarFill } from 'react-icons/bs';
-import { Message, Select } from '../UsedInputs';
-import Rating from '../Stars';
-import { UsersData } from '../../Data/MovieData';
+import React, { useEffect } from "react";
+import Titles from "../Titles";
+import { BsBookmarkStarFill } from "react-icons/bs";
+import { Message, Select } from "../UsedInputs";
+import Rating from "../Stars";
+import { Empty } from "../Notfications/Empty";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ReviewValidation } from "../Validation/MovieValidation";
+import toast from "react-hot-toast";
+import { InlineError } from "../Notfications/Error";
+import { Link } from "react-router-dom";
+import { reviewMovieAction } from "../../Redux/Actions/MoviesActions";
+
+const Ratings = [
+  {
+    title: "0 - Жах",
+    value: 0,
+  },
+  {
+    title: "1 - Погано",
+    value: 1,
+  },
+  {
+    title: "2 - Нормально",
+    value: 2,
+  },
+  {
+    title: "3 - Досить непогано",
+    value: 3,
+  },
+  {
+    title: "4 - Відмінно",
+    value: 4,
+  },
+  {
+    title: "5 - Крутяк",
+    value: 5,
+  },
+];
 
 function MovieRates({ movie }) {
-  const Ratings = [
-    {
-      title: '0 - Жах',
-      value: 0,
-    },
-    {
-      title: '1 - Погано',
-      value: 1,
-    },
-    {
-      title: '2 - Нормально',
-      value: 2,
-    },
-    {
-      title: '3 - Досить непогано',
-      value: 3,
-    },
-    {
-      title: '4 - Відмінно',
-      value: 4,
-    },
-    {
-      title: '5 - Крутяк',
-      value: 5,
-    },
-  ];
+  const dispatch = useDispatch();
+  const { isLoading, isError } = useSelector((state) => state.createReview);
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(ReviewValidation),
+  });
+  const onSubmit = (data) => {
+    dispatch(
+      reviewMovieAction({
+        id: movie?._id,
+        review: data,
+      })
+    );
+  };
 
-  const [rating, setRating] = React.useState();
+  useEffect(() => {
+    if (isError) {
+      toast.error(isError);
+      dispatch({ type: "CREATE_REVIEW_RESET" });
+    }
+  }, [isError, dispatch]);
 
   return (
     <div className="my-12">
-      <Titles title="Відгук" Icon={BsBookmarkStarFill} />
+      <Titles title="Відгуки" Icon={BsBookmarkStarFill} />
       <div className="mt-10 xl:grid flex-colo grid-cols-5 gap-12 bg-dry xs:p-10 py-10 px-2 sm:p-20 rounded">
-        {/* write review */}
-        <div className="xl:col-span-2 w-full flex flex-col gap-8">
-          <h3 className="text-xl text-text font-semibold">Відгук "{movie?.name}"</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="xl:col-span-2 w-full flex flex-col gap-8"
+        >
+          <h3 className="text-xl text-text font-semibold">
+            Рецензія "{movie?.name}"
+          </h3>
           <p className="text-sm leading-7 font-medium text-border">
-            Напишіть рецензію на цей фільм. Він буде опублікований на цій сторінці.
+            Напишіть рецензію на цей фільм. Він буде розміщений на цій сторінці.
           </p>
           <div className="text-sm w-full">
             <Select
-              label="Обрати оцінку"
+              label="Виберіть рейтинг"
               options={Ratings}
-              onChange={(e) => setRating(e.target.value)}
+              name="rating"
+              register={{ ...register("rating") }}
             />
             <div className="flex mt-4 text-lg gap-2 text-star">
-              <Rating value={rating} />
+              <Rating value={watch("rating", false)} />
             </div>
+            {errors.rating && <InlineError text={errors.rating.message} />}
           </div>
-          <Message label="Повідомлення" placeholder="Коротко і ясно" />
-          <button className="bg-subMain text-white py-3 w-full flex-colo rounded">
-            Підвердити
-          </button>
-        </div>
-        <div className="col-span-3 flex flex-col gap-6">
-          <h3 className="text-xl text-text font-semibold">Відгуки (56)</h3>
+          <div className="w-full">
+            <Message
+              name="comment"
+              register={{ ...register("comment") }}
+              label="Повідомлення"
+              placeholder="Коротко і ясно"
+            />
+            {errors.comment && <InlineError text={errors.comment.message} />}
+          </div>
+          {userInfo ? (
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-subMain text-white py-4 w-full flex-colo rounded"
+            >
+              {isLoading ? "Завантаження..." : "Надіслати"}
+            </button>
+          ) : (
+            <Link
+              to="/login"
+              className="bg-main border border-dashed border-border text-subMain py-4 w-full flex-colo rounded"
+            >
+              Увійдіть, щоб переглянути цей фільм
+            </Link>
+          )}
+        </form>
+        <div className="col-span-3 flex w-full flex-col gap-6">
+          <h3 className="text-xl text-text font-semibold">
+            Відгуки ({movie?.numberOfReviews})
+          </h3>
           <div className="w-full flex flex-col bg-main gap-6 rounded-lg md:p-12 p-6 h-header overflow-y-scroll">
-            {UsersData.map((user, i) => (
-              <div className="md:grid flex flex-col w-full grid-cols-12 gap-6 bg-dry p-4 border border-gray-800 rounded-lg">
-                <div className="col-span-2 bg-main hidden md:block">
-                  <img
-                    src={`/images/${user ? user.image : 'user.jpg'}`}
-                    alt={user.fullName}
-                    className="w-full h-24 rounded-lg object-cover"
-                  />
+            {movie?.reviews?.length > 0 ? (
+              movie?.reviews?.map((review) => (
+                <div
+                  key={review?._id}
+                  className="md:grid flex flex-col w-full grid-cols-12 gap-6 bg-dry p-4 border border-gray-800 rounded-lg"
+                >
+                  <div className="col-span-2 bg-main hidden md:block">
+                    <img
+                      src={
+                        review?.userImage
+                          ? review.userImage
+                          : "/images/user.jpg"
+                      }
+                      alt={review?.userName}
+                      className="w-full h-24 rounded-lg object-cover"
+                    />
+                  </div>
+                  <div className="col-span-7 flex flex-col gap-2">
+                    <h2>{review?.userName}</h2>
+                    <p className="text-xs leading-6 font-medium text-text">
+                      {review?.comment}
+                    </p>
+                  </div>
+                  <div className="col-span-3 flex-rows border-l border-border text-xs gap-1 text-star">
+                    <Rating value={review?.rating} />
+                  </div>
                 </div>
-                <div className="col-span-7 flex flex-col gap-2">
-                  <h2>{user?.fullName}</h2>
-                  <p className="text-xs leading-6 font-medium text-text">{user?.message}</p>
-                </div>
-                <div className="col-span-3 flex-rows border-l border-border text-xs gap-1 text-star">
-                  <Rating value={user?.rate} />
-                </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <Empty message={`Будьте першим, хто оцінить "${movie?.name}"`} />
+            )}
           </div>
         </div>
       </div>
@@ -89,3 +164,10 @@ function MovieRates({ movie }) {
 }
 
 export default MovieRates;
+
+
+
+
+
+
+
